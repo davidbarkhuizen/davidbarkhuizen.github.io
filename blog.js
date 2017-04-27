5f40c8d0cdfb016bec71237d80b257375cf5b30b
david barkhuizen <david.barkhuizen@gmail.com>
2017-04-27 18:23:31 +0200

    js load

---
+++ blog.js
@@ -0,0 +1,5 @@
+var host = window.location.host;
+var origin = window.location.origin;
+var path = window.location.pathname;
+
+alert(`host ${host}, origin ${origin}, path ${path}`);
--- src/blog.js
+++
@@ -1,1 +0,0 @@
-alert('jou ma se...');
