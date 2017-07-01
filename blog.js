fdf2552e7ab2740206af14790709990cd172297f
david barkhuizen <david.barkhuizen@gmail.com>
2017-07-01 19:57:21 +0200

    removed alert

--- blog.js
+++ blog.js
@@ -2,4 +2,30 @@ var host = window.location.host;
 var origin = window.location.origin;
 var path = window.location.pathname;
 
-alert(`host ${host}, origin ${origin}, path ${path}`);
+console.log(`host ${host}, origin ${origin}, path ${path}`);
+
+var GET_JSON_OPTIONS = { method: 'GET', body: 'json', headers: {} };
+
+var index
+var index = [];
+
+function bindIndexFromJson(response) {
+	return response.json();
+}
+
+function load() {
+
+	var request = new Request('index.json', {
+		method: 'GET',
+		headers: new Headers({
+			'Content-Type': 'application/json'
+		})
+	});
+
+
+	fetch(request)
+	.then(bindIndexFromJson)
+	.then(function(j){ console.log(j); });
+}
+
+load();
