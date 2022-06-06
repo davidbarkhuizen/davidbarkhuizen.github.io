5d5db9c029fd8fdf9e79118f3a33818ca7bffd8d
davidbarkhuizen <david.barkhuizen@gmail.com>
2022-06-06 12:57:14 +0200

    fetch index, display entry titles

--- index.js
+++ index.js
@@ -6,22 +6,36 @@ console.log(`host ${host}, origin ${origin}, path ${path}`);
 
 var GET_JSON_OPTIONS = { method: 'GET', body: 'json', headers: {} };
 
-var index
 var index = [];
 
-function bindIndexFromJson(response) {
-	return response.json();
+const bindIndexFromJson = async (response) => {
+
+	const list_element = document.getElementById('nav-panel');
+
+	const entries = await response.json();
+	
+	entries.forEach(entry => {
+		
+		const list_item = document.createElement("li");
+		const item_text = document.createTextNode(entry.title);
+		list_item.appendChild(item_text);
+
+		console.log(list_item);
+		
+		list_element.appendChild(list_item);
+	});
+	
+	return entries;
 }
 
 function load() {
 
-	var request = new Request('index.json', {
+	const request = new Request('index.json', {
 		method: 'GET',
 		headers: new Headers({
 			'Content-Type': 'application/json'
 		})
 	});
-
 
 	fetch(request)
 	.then(bindIndexFromJson)
