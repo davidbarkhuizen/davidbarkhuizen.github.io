687b07686b561d60bd1dfa14df7e573a05c47da6
david barkhuizen <david.barkhuizen@gmail.com>
2022-06-12 14:15:49 +0200

    wip

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var path = window.location.pathname;
 
 console.log(`host ${host}, origin ${origin}, path ${path}`);
 
+var dataModel = null;
+const onClicks = [];
+
 const getElementRefs = () => {
 
 	const getRef = (id) => document.getElementById(id);
@@ -52,6 +55,52 @@ const renderAndSetEntry = (entry, text, mdReader, mdWriter) => {
 	element.contentPanelBody.innerHTML = renderedHTML;				
 };
 
+const onclick = async (entry, mdReader, mdWriter) => {
+	
+	const response = await get(`/entries/${entry.url}`, CONTENT_TYPES.TEXT_PLAIN);
+	
+	const entry_data_reader = response.body.getReader();
+
+	var data_buffer = new Uint8Array(0);
+
+	const onAllEntryDataAvailable = async () => {
+		const text = decoder.decode(data_buffer);
+
+		const currentlySelectedIndexItem = document.querySelectorAll(`.selected.index-item`)[0];
+		if (currentlySelectedIndexItem) {
+			currentlySelectedIndexItem.classList.remove('selected');
+		}				
+
+		const toSelect = document.querySelectorAll(`.index-item[x-index-title='${entry.title}']`)[0]
+		toSelect.classList.add('selected');
+
+		renderAndSetEntry(entry, text, mdReader, mdWriter);
+	};
+
+	const decoder = new TextDecoder();
+
+	const processEntryData = async () => {
+
+		const reader_rsp = await entry_data_reader.read();
+		
+		const new_data = reader_rsp.value;
+		if (new_data) {
+			const merged = new Uint8Array(data_buffer.length + new_data.length);
+			merged.set(data_buffer);
+			merged.set(new_data, data_buffer.length);
+			data_buffer = merged
+		}
+
+		if (reader_rsp.done) {
+			await onAllEntryDataAvailable();
+		} else {
+			await processEntryData();
+		}
+	}
+
+	await processEntryData()
+};
+
 const renderIndexItem = (entry, mdReader, mdWriter) => {
 
 	const tooltip = 
@@ -63,82 +112,56 @@ ${entry.summary}
 	const template = 
 `<div class="index-item" x-index-title="${entry.title}" title="${tooltip}">
 	<span>${entry.title}<span>
-</div>`;
-
-	const onclick = async () => {
-	
-		const response = await get(`/entries/${entry.link}`, CONTENT_TYPES.TEXT_PLAIN);
-		
-		const entry_data_reader = response.body.getReader();
-
-		var data_buffer = new Uint8Array(0);
-
-		const onAllEntryDataAvailable = async () => {
-			const text = decoder.decode(data_buffer);
-
-			const currentlySelectedIndexItem = document.querySelectorAll(`.selected.index-item`)[0];
-			if (currentlySelectedIndexItem) {
-				currentlySelectedIndexItem.classList.remove('selected');
-			}				
-
-			const toSelect = document.querySelectorAll(`.index-item[x-index-title='${entry.title}']`)[0]
-			toSelect.classList.add('selected');
-
-			renderAndSetEntry(entry, text, mdReader, mdWriter);
-		};
-
-		const decoder = new TextDecoder();
-
-		const processEntryData = async () => {
-
-			const reader_rsp = await entry_data_reader.read();
-			
-			const new_data = reader_rsp.value;
-			if (new_data) {
-				const merged = new Uint8Array(data_buffer.length + new_data.length);
-				merged.set(data_buffer);
-				merged.set(new_data, data_buffer.length);
-				data_buffer = merged
-			}
-
-			if (reader_rsp.done) {
-				await onAllEntryDataAvailable();
-			} else {
-				await processEntryData();
-			}
-		}
-
-		await processEntryData()
-	};		
+</div>`;	
 
 	const item = document.createElement('div');
 	item.innerHTML = template;
-	item.onclick = onclick;
+	
+	const itemOnClick = () => { onclick(entry, mdReader, mdWriter); };
+	
+	item.onclick = itemOnClick;
 	element.navPanel.appendChild(item);
 
-	return onclick;
+	onClicks.push(itemOnClick);
 };
 
-const bindMetaData = (metaData, mdReader, mdWriter) => {
+const renderNavPanel = (dataModel, mdReader, mdWriter) => {
 
-	document.title = metaData.title;
-
-	return metaData.entries
-		.sort(item => new Date(item.date))
-		.reverse()
+	dataModel.entries
 		.map(entry => renderIndexItem(entry, mdReader, mdWriter));
+};
+
+const bindDataModel = (dataModel, mdReader, mdWriter) => {
+
+	document.title = dataModel.title;
+
+	dataModel.entries.sort(item => new Date(item.date))
+
+	dataModel.entries
+		.sort(item => new Date(item.date))
+		.reverse();
+
+	window.dataModel = dataModel;
+
+	renderNavPanel(dataModel, mdReader, mdWriter);
 }
 
-const fetchinitialDataModel = async () => { 
+const fetchDataModel = async () => { 
 	const rsp = await get('index.json', CONTENT_TYPES.APP_JSON);
 	return await rsp.json();
 };
 
-const enter = (mdReader, mdWriter) => 
-	fetchinitialDataModel()
-	.then((data) => bindMetaData(data, mdReader, mdWriter))
-	.then((onclicks) => onclicks[0]());
+const processQueryString = async () => {
+	
+	
+	
+	
+	onClicks[0]();
+};
 
-// enter();
+const enter = (mdReader, mdWriter) => 
+	fetchDataModel()
+	.then((dataModel) => bindDataModel(dataModel, mdReader, mdWriter))
+	.then(processQueryString);
 
 window.enter = enter;
